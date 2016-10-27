cb7a4cf372caf2ce2aa5d8444a2564ee317b555a
cj222jb <cj222jb@student.lnu.se>
2016-10-27 15:56:59 +0200

    testingCode

--- BlackJack/model/rules/BasicHitStrategy.java
+++ BlackJack/model/rules/BasicHitStrategy.java
@@ -6,6 +6,9 @@ class BasicHitStrategy implements IHitStrategy {
     private final int g_hitLimit = 17;
 
     public boolean DoHit(Player a_dealer) {
-      return a_dealer.CalcScore() < g_hitLimit;  
+        if(a_dealer.hasSoftSeventeen()){
+            System.out.println("trueueueue");
+        }
+      return a_dealer.CalcScore() < g_hitLimit || a_dealer.hasSoftSeventeen();
     }
 }
